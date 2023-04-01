fbac64d347f7758ff601c1042f3677424c266245
leotofoli <leoadt@gmail.com>
2023-04-01 12:24:37 -0300

    Criação do SideBar do video e preparação da aplicação para receber informações do banco de dados

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+        likes={100}
+        messages={200}
+        shares={300} 
+        name="apenas_um_teste"
+        description="Meditação"
+        music="zen"
+        url="https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2"/>
+        <Video 
+        likes={111}
+        messages={222}
+        shares={333} 
+        name="Teste"
+        description="Meditação"
+        music="zen"
+        url="https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2"/>
       </div>
     </div>
   );
